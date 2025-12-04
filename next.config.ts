6eca82e591f9e67681caef5e6fffe1a9a6535131
Roman Balan <RBalan@ibs.ru>
2025-12-05 00:21:32 +0100

    fix

--- next.config.ts
+++ next.config.ts
@@ -1,29 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
+  output: "export", // Commented out to enable SSR for dynamic routes
   images: {
     loader: "cloudinary",
     path: "https://res.cloudinary.com/YOUR_CLOUD_NAME/image/upload",
     unoptimized: true,
   },
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.(png|jpg|jpeg|gif|svg)$/i,
-      use: [
-        {
-          loader: "next-image-export-optimizer",
-          options: {
-            // You can specify the quality of the optimized images
-            quality: 75,
-            // You can specify the output directory for optimized images
-            outputPath: "public/optimized-images",
-          },
-        },
-      ],
-    });
-    return config;
-  },
+
   /* config options here */
 };
 
